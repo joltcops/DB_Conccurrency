acc3ffb5f5ea8bd9188d2b387c5b016b28d51ce8
joltcops <pracheta@kgpian.iitkgp.ac.in>
2025-04-14 13:24:57 +0530

    Whoops

--- concMan/test03.cpp
+++ concMan/test03.cpp
@@ -4,60 +4,6 @@
 #include <chrono>
 #include <print>
 
-// void t1(LockManager& lm, int tid) {
-//     try {
-//         lm.begin_transaction(tid);
-//         int tl = lm.try_lock(tid, 0, false);
-//         if(tl == -1) {
-//             std::println(">> Transaction {} cannot acquire write lock on resource 0", tid);
-//             lm.write_lock(tid, 0);
-//         }
-//         else if (tl == 0) {
-//             std::println(">> Transaction {} can acquire write lock on resource 0", tid);
-//             lm.write_lock(tid, 0);
-//         }
-//         else{
-//             std::println(">> Transaction {} acquired write lock on resource 0", tid);
-//         }
-
-//         std::println(">> t1 acquired write lock for resource 0");
-//         std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-//         lm.write_lock(tid, 1);
-//         std::println(">> t1 acquired read lock for resource 1");
-//         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-//         lm.finish_transaction(tid);
-//     } catch (const std::exception& e) {
-//         std::println("Transaction {} error: {}", tid, e.what());
-//     }
-// }
-
-// void t2(LockManager& lm, int tid) {
-//     try {
-//         lm.begin_transaction(tid);
-//         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-//         int tl = lm.try_lock(tid, 0, true);
-//         if (tl == -1) {
-//             std::println(">> Transaction {} cannot acquire read lock on resource 0", tid);
-//             lm.write_lock(tid, 0);
-//         }
-//         else if (tl == 0) {
-//             std::println(">> Transaction {} can acquire read lock on resource 0", tid);
-//             lm.write_lock(tid, 0);
-//         }
-//         else{
-//             std::println(">> Transaction {} acquired read lock on resource 0", tid);
-//         }
-//         std::println(">> t2 acquired read lock for resource 0");
-//         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-//         lm.write_lock(tid, 1);
-//         std::println(">> t2 acquired read lock for resource 1");
-//         std::this_thread::sleep_for(std::chrono::milliseconds(10000));
-//         lm.finish_transaction(tid);
-//     } catch (const std::exception& e) {
-//         std::println(">> Transaction {} error: {}", tid, e.what());
-//     }
-// }
-
 void t0(LockManager& lm, int tid) {
     try {
         lm.begin_transaction(tid);
@@ -66,8 +12,6 @@ void t0(LockManager& lm, int tid) {
         lm.write_lock(tid, 2);
         std::println(">> Transaction {} acquired write lock on resource 2", tid);
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        // lm.finish_transaction(tid);
-        // std::println(">> Transaction {} has finished", tid);
         std::println(">> Transaction {} is trying to acquire write lock on resource 0", tid);
         lm.write_lock(tid, 0);
         std::println(">> Transaction {} acquired write lock on resource 0", tid);
