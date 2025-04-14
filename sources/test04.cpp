9154658ffa54cbeab2dfcaf0e2f9ec7887a7b5dc
Gayathri2004Anant <gayathrianant05@gmail.com>
2025-04-14 17:50:44 +0530

    final??

--- sources/test04.cpp
+++ sources/test04.cpp
@@ -4,13 +4,17 @@
 #include <chrono>
 #include <print>
 
-// a linear chain of requests.
+// a linear chain of requests + try_lock
 
 void t0(LockManager& lm, int tid) {
     try {
         lm.begin_transaction(tid);
         std::println(">> Transaction {} has started", tid);
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        int op = lm.try_lock(tid, 0, 0);
+        if(op == 1) std::println(">> Trylock: Transaction {} acquired write lock on resource 0", tid);
+        else if(op == 0) std::println(">> Trylock: Transaction {} cannot acquire write lock on resource 0 immediately", tid);
+        else std::println(">> Trylock: Transaction {} cannot acquire write lock on resource 0 - leads to deadlock", tid);
         std::println(">> Transaction {} is trying to acquire write lock on resource 0", tid);
         lm.write_lock(tid, 0);
         std::println(">> Transaction {} acquired write lock on resource 0", tid);
@@ -26,8 +30,10 @@ void t1(LockManager& lm, int tid) {
         lm.begin_transaction(tid);
         std::println(">> Transaction {} has started", tid);
         std::println(">> Transaction {} is trying to acquire write lock on resource 0", tid);
-        lm.write_lock(tid, 0);
-        std::println(">> Transaction {} acquired write lock on resource 0", tid);
+        int op = lm.try_lock(tid, 0, 0);
+        if(op == 1) std::println(">> Trylock: Transaction {} acquired write lock on resource 0", tid);
+        else if(op == 0) std::println(">> Trylock: Transaction {} cannot acquire write lock on resource 0 immediately", tid);
+        else std::println(">> Trylock: Transaction {} cannot acquire write lock on resource 0 - leads to deadlock", tid);
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
         std::println(">> Transaction {} is trying to acquire write lock on resource 1", tid);
         lm.write_lock(tid, 1);
